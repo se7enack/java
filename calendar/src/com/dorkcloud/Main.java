be7cbe1609b4387287cf80439fb67ee44f29792d
Stephen Burke <y2kitguy@gmail.com>
2016-10-10 22:43:37 -0400

    calendar

---
+++ calendar/src/com/dorkcloud/Main.java
@@ -0,0 +1,11 @@
+package com.dorkcloud;
+
+import java.util.Calendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calendar myCal = Calendar.getInstance();
+        System.out.println(myCal.getTime());
+    }
+}
